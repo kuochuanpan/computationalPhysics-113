a3f74b87832d4714be5ab3970373c114616c8e27
K.-C. Pan <kuochuan.pan@gapp.nthu.edu.tw>
2024-10-18 08:55:42 +0800

    files for the project 2

---
+++ project2/nbody/particles.py
@@ -0,0 +1,12 @@
+import numpy as np
+import matplotlib.pyplot as plt
+
+
+class Particles:
+    """
+    Particle class to store particle properties
+    """
+    def __init__(self, N):
+        self.nparticles = N
+        return
+
